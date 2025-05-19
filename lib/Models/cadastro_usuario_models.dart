35dbcf367a07656a9835d181b8ff46e463537cda
MateusLemes <mateuslfn00@gmail.com>
2025-05-19 19:26:52 -0400

    adicionar tela de cadastro v1

---
+++ lib/Models/cadastro_usuario_models.dart
@@ -0,0 +1,27 @@
+class UsuarioModel {
+  String id;
+  String nome;
+  String email;
+  String senha;
+
+  UsuarioModel(
+      {required this.id,
+      required this.nome,
+      required this.email,
+      required this.senha});
+
+  UsuarioModel.fromMap(Map<String, dynamic> map)
+      : id = map["id"],
+        nome = map["nome"],
+        email = map["email"],
+        senha = map["senha"];
+  
+  Map<String, dynamic> toMap(){
+    return {
+      "id": id,
+      "nome": nome,
+      "email": email,
+      "senha": senha,
+    };
+  }
+}
---
+++ lib/Pages/cadastro_usuario_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CadastroUsuarioPage extends StatelessWidget {
+  const CadastroUsuarioPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    Scaffold(
+      appBar: AppBar(title: const Text("Cadastro de Usuario"), centerTitle: true,),
+      body:
+      Center(child:
+        Padding(padding: const EdgeInsets.all(16.0),child: 
+          Form(child: 
+          Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [ 
+                const SizedBox(height: 32,),
+                TextFormField(decoration: const InputDecoration(label: Text("Nome:")),),
+                TextFormField(decoration: const InputDecoration(label: Text("E-mail:")),),
+                TextFormField(decoration: const InputDecoration(label: Text("Senha:")),),
+                TextFormField(decoration: const InputDecoration(label: Text("Confirme a Senha:")),),
+                const SizedBox(height: 16,),
+                ElevatedButton(onPressed: (){}, child: const Text("Cadastrar")),
+                const Divider(),
+                TextButton(onPressed: (){}, child: const Text("Já tem uma conta? Entre!")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
