d101ab439a78ee0c4b8bce6805baf7bd1d701c90
Mateus <mateuslfn00@gmail.com>
2025-06-30 20:27:16 -0400

    cadastro bonito

--- lib/Pages/cadastro_usuario_page.dart
+++ lib/Pages/cadastro_usuario_page.dart
@@ -19,109 +19,167 @@ class _CadastroUsuarioPageState extends State<CadastroUsuarioPage> {
       TextEditingController();
   final AutenticacaoServico _autServico = AutenticacaoServico();
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Cadastro de Usuario"),
+        title: const Text("Cadastro de Usuário"),
         centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 28, 58, 226),
+        elevation: 4,
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 32,
-                ),
-                TextFormField(
-                  controller: _nomeController,
-                  decoration: const InputDecoration(label: Text("Nome:")),
-                  validator: (String? value) {
-                    if (value == null) {
-                      return "O nome não pode estar vazio";
-                    }
-                    if (value.length < 5) {
-                      return "O nome é muito curto";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(label: Text("E-mail:")),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Digite um e-mail';
-                    }
-                    if (!value.contains('@')) return 'E-mail inválido';
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _senhaController,
-                  decoration: const InputDecoration(label: Text("Senha:")),
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null) {
-                      return "A senha não pode estar vazia";
-                    }
-                    if (value.length < 5) {
-                      return "A senha esta muito curta";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _confirmaSenhaController,
-                  decoration:
-                      const InputDecoration(label: Text("Confirme a Senha:")),
-                  obscureText: true,
-                  validator: (String? value) {
-                    if (value == null) {
-                      return "A Confirmção de senha não pode estar vazia";
-                    }
-                    if (value.length < 5) {
-                      return "A Confirmção de senha é muito curta";
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      String nome = _nomeController.text;
-                      String email = _emailController.text;
-                      String senha = _senhaController.text;
-                      print(
-                          "Usuário cadastrado: ${_emailController.text}, ${_senhaController.text}, ${_nomeController.text}, ");
-                      _autServico.cadastrarUsuario(
-                          nome: nome, senha: senha, email: email);
-                      // Aqui você pode chamar um método para enviar os dados ao backend
-                    }
-                  },
-                  child: const Text("Cadastrar"),
-                ),
-                const Divider(),
-                TextButton(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  Text(
+                    "Cadastre-se agora",
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          color: const Color.fromARGB(255, 45, 106, 236),
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  _buildTextField(
+                    controller: _nomeController,
+                    label: "Nome",
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "O nome não pode estar vazio";
+                      }
+                      if (value.length < 5) {
+                        return "O nome é muito curto";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  _buildTextField(
+                    controller: _emailController,
+                    label: "E-mail",
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Digite um e-mail';
+                      }
+                      if (!value.contains('@')) return 'E-mail inválido';
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  _buildTextField(
+                    controller: _senhaController,
+                    label: "Senha",
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "A senha não pode estar vazia";
+                      }
+                      if (value.length < 5) {
+                        return "A senha está muito curta";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  _buildTextField(
+                    controller: _confirmaSenhaController,
+                    label: "Confirme a Senha",
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "A confirmação de senha não pode estar vazia";
+                      }
+                      if (value != _senhaController.text) {
+                        return "As senhas não coincidem";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        String nome = _nomeController.text;
+                        String email = _emailController.text;
+                        String senha = _senhaController.text;
+                        print("Usuário cadastrado: $nome, $email");
+                        _autServico.cadastrarUsuario(
+                            nome: nome, senha: senha, email: email);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 224, 20, 214),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text(
+                      "Cadastrar",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Divider(),
+                  const SizedBox(height: 16),
+                  TextButton(
                     onPressed: () {
                       print("foi pro login");
                     },
-                    child: const Text("Já tem uma conta? Entre!")),
-              ],
+                    child: const Text(
+                      "Já tem uma conta? Entre!",
+                      style: TextStyle(
+                          color: Color.fromARGB(234, 240, 45, 240),
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
-}
 
-extension on GlobalKey<FormState> {}
+  // Função que centraliza o design do TextFormField
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String label,
+    bool obscureText = false,
+    TextInputType keyboardType = TextInputType.text,
+    String? Function(String?)? validator,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Color.fromARGB(255, 226, 34, 201)),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 18, horizontal: 20),
+      ),
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      validator: validator,
+    );
+  }
+}
